0bbadc271cec39d2d7af973c93f15cdfb859af0a
anzer-farooq <musabibnanzer@gmail.com>
2026-09-05 15:34:22 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+       int n=temperatures.size();
+       vector<int> res(n); 
+       stack<int> st;
+       res[n-1]=0;
+       st.push(n-1);
+       for(int i=n-2;i>=0;i--){
+        while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
+            st.pop();
+        }
+        if(!st.empty()){
+        res[i]=st.top()-i;
+        
+        }
+        else{
+            res[i]=0;
+        }st.push(i);
+       }
+       return res;
+    }
+};
